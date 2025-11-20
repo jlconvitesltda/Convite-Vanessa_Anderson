71fb9c0c6b100974688e9af9edad5e50ec05e008
jlconvitesltda <jlconvitesltda@gmail.com>
2025-11-20 18:41:36 -0300

    Update FloralBackground.tsx

--- components/FloralBackground.tsx
+++ components/FloralBackground.tsx
@@ -8,7 +8,7 @@ const FloralBackground: React.FC<FloralBackgroundProps> = ({ children }) => {
   return (
     <div className="relative w-full min-h-screen flex flex-col">
       {/* Top Floral Border (Image Simulation) */}
-      <div className="absolute top-0 left-0 w-full h-32 md:h-48 overflow-hidden z-0 pointer-events-none">
+      <div className="absolute top-0 left-0 w-full h-20 md:h-32 overflow-hidden z-0 pointer-events-none">
         <img 
           src="/borda-abaixo.jpg" 
           alt="Purple Flowers Top"
